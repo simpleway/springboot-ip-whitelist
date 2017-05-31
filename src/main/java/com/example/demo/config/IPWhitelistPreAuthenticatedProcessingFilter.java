040a8c5a9b41b2fd2ee2c46bea5f2705326ade8b
Wei Shen <wei_shen@yahoo.com>
2017-05-30 23:41:48 -0600

    cleanup unused import

--- src/main/java/com/example/demo/config/IPWhitelistPreAuthenticatedProcessingFilter.java
+++ src/main/java/com/example/demo/config/IPWhitelistPreAuthenticatedProcessingFilter.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
